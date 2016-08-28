ef3022dd52df1feb1dde2b187fd8645337fe4eb9
Jasper S <jack.shoemaker112233@gmail.com>
2016-08-28 19:52:47 +0700

    add root route for prod health checks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  #add root route for prod health check
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
